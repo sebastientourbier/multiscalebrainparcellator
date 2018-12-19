6d362993aab2557dee28e8409eba70b3b328eb8e
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2018-12-19 14:31:55 +0100

    FIX: edits of DOC

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,7 @@ source_suffix = ['.rst', '.md']
 extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.mathjax',
               'sphinx.ext.viewcode',
-              'sphinx.ext.argparse']
+              'sphinxarg.ext']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
