eff860bb8454f139e1959aee2a95a7eed222b483
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2019-01-10 09:26:35 +0100

    Dev merged into master

--- cmp/multiscalebrainparcellator/info.py
+++ cmp/multiscalebrainparcellator/info.py
@@ -5,7 +5,7 @@ _version_minor = 0
 _version_micro = 0
 _version_extra = '-beta'
 
-__release_date__ = '2018.11.19'
+__release_date__ = '2019.01.10'
 
 __minor_version__ = "%s.%s" % (_version_major,
                                _version_minor)
