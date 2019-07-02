af14619b17c6af0fbf807d072de598eb0c0cc648
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2019-07-02 13:26:18 +0200

    ENH: check version of pybids > 0.6.4

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ def main(**extra_args):
           maintainer = 'Brain Communication Pathways Sinergia Consortium',
           maintainer_email = 'sebastien.tourbier@alumni.epfl.ch',
           package_data = package_data,
-          requires=["numpy (>=1.2)", "nibabel (>=2.0.0)"],
+          requires=["numpy (>=1.2)", "nibabel (>=2.0.0)", "pybids (>=0.6.4)"],
           **extra_args
          )
 
