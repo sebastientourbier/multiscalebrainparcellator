2cb227d412148e92b760268c018fab8c498503c6
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2019-08-22 14:57:28 +0200

    MAINT: move bcolors utility class to cmtklib.util

--- cmtklib/util.py
+++ cmtklib/util.py
@@ -8,6 +8,18 @@
 
 import numpy as np
 
+class bcolors:
+    """ Utility class for color unicode
+    """
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 def length(xyz, along=False):
     """
     Euclidean length of track line
